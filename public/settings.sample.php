e74ebebb42f753219f5846fcc2c33f3ca8d7a5c9
fellowgeek <info@erfan.me>
2023-10-21 14:05:53 -0600

    settings sample update

--- public/settings.sample.php
+++ public/settings.sample.php
@@ -1,37 +1,40 @@
 <?php
 
-// OhCRUD settings
+// Define settings for the OhCRUD application
 
-// path to application (normalized for nginx and apache)
+// Define the base path for the application, normalized for both Nginx and Apache.
 define('__SELF__', __DIR__ . '/');
 
-// application url
+// Define the application's URL based on the server's server name, or an empty string if not available.
 define('__SITE__', $_SERVER['SERVER_NAME'] ?? '');
 
-// debug mode ( set to false for production )
+// Define the name of the application.
+define('__APP__', 'Oh CRUD!');
+
+// Enable or disable debug mode (set to false for production use).
 define('__OHCRUD_DEBUG_MODE__', true);
 
-// debug method initially expanded levels (for HTML mode only)
+// Specify the initial expanded debug levels for debugging (HTML mode only).
 define('__OHCRUD_DEBUG_EXPANDED_LEVEL__', 3);
 
-// logs
+// Configure logging settings.
 define('__OHCRUD_LOG_ENABLED__', true);
 define('__OHCRUD_LOG_FILE__', __DIR__ . '/app.log');
 
-// cache settings
+// Configure caching settings.
 define('__OHCRUD_CACHE_ENABLED__', false);
 define('__OHCRUD_CACHE_PATH__', '/ramdisk/');
 
-// secret used for password hash ( should be unique for each application )
+// Define the secret used for password hashing (should be unique for each application).
 define('__OHCRUD_SECRET__', 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX');
 
-// session lifetime in seconds
+// Set the session lifetime in seconds.
 define('__OHCRUD_SESSION_LIFETIME__', 3600);
 
-// should OhCRUD add (CDATE, MDATE, CUSER, MUSER) columns to every table
+// Determine whether OhCRUD should add (CDATE, MDATE, CUSER, MUSER) columns to every table.
 define('__OHCRUD_DB_STAMP__', true);
 
-// database settings ( driver can be SQLITE or MYSQL )
+// Configure the database settings, specifying the driver (can be SQLITE or MYSQL).
 define('__OHCRUD_DB_CONFIG__', serialize([
         'DRIVER' => 'MYSQL',
         'PERSISTENT_CONNECTION' => false,
@@ -47,6 +50,6 @@ define('__OHCRUD_DB_CONFIG__', serialize([
 // IMPORTANT
 // API end points defined in routes.php
 
-// CMS settings
+// Define settings for the Content Management System (CMS).
 define('__OHCRUD_CMS_DEFAULT_THEME__', 'default');
 define('__OHCRUD_CMS_DEFAULT_LAYOUT__', 'index');
